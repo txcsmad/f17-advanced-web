cb4c0a77e9d1df4a311df15ffb9a57c5daf1b842
Drew Romanyk <drewiswaycool@gmail.com>
2017-10-16 15:44:36 -0500

    Workshop #2 End Result

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class TextPage extends Component {
 
   render() {
     return (
-      <h1></h1>
+      <h1>{this.props.match.params.text}</h1>
     );
   }
 }
@@ -43,7 +43,8 @@ class SquareButton extends Component {
   render() {
     // TODO
     return (
-      <div className="button-square">
+      <div className="button-square" onClick={() => this.props.handleOnClick(this.props.x, this.props.y)}>
+        {this.props.value}
       </div>
     );
   }
@@ -61,17 +62,39 @@ class GamePage extends Component {
 
   getWinner(board) {
     //TODO return player that won
+    if (board[0] !== "" && board[0] === board[1] && board[1] === board[2]) {
+      return board[0];
+    }
+    return null;
   }
 
 
   onChangeState(x, y) {
     // TODO
+    if (this.state.gameWinner !== null || this.state.board[y * 3 + x] !== "") {
+      return;
+    }
+
+    let newBoard = this.state.board.splice(0);
+    newBoard[y * 3 + x] = this.state.currentPlayer === 0 ? "X" : "O";
+
+    this.setState({
+      board: newBoard,
+      currentPlayer: this.state.currentPlayer === 0 ? 1 : 0,
+      gameWinner: this.getWinner(newBoard)
+    });
   }
 
   render() {
+    let array = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+
     return (
       <div>
+        {this.state.gameWinner !== null ? <h1>Winner: {this.state.gameWinner}</h1> : <p/>}
         <div className="board">
+          {array.map((value) => (<SquareButton x={value % 3} y={value / 3} value={this.state.board[value]}
+                                               handleOnClick={(x, y) => this.onChangeState(x, y)}/>))}
         </div>
       </div>
     );
@@ -82,22 +105,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <TextPage text={"Hello"}/>
-            {
-              //TODO Links go here
-              // / = Home
-              // /header/(some_text) = Some Text
-              // /game = Game
-              // /notfound = Not Found
-            }
-          </div>
-          {
-            //TODO Switch goes here
-          }
-        </div>
+        <BrowserRouter>
+          <Switch>
+            <div>
+              <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <TextPage text={"Hello"}/>
+                {
+                  //TODO Links go here
+                  // / = Home
+                  // /header/(some_text) = Some Text
+                  // /game = Game
+                  // /notfound = Not Found
+                }
+                <Route exact path="/" component={TextPage}/>
+                <Route path="/text/:text" component={TextPage}/>
+                <Route path="/game" component={GamePage}/>
+                <Route component={NotFoundPage}/>
+              </div>
+              {
+                //TODO Switch goes here
+              }
+            </div>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
